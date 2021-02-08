c426baf51d34332d2e2f1088dd56f6bf4e6d4799
Edevar Bastreghi Filho <edevarfilho@gmail.com>
2021-02-08 07:48:35 -0300

    Checkout entity
    Checkout entity

---
+++ checkout/src/main/java/br/ecommerce/checkout/entity/CheckoutEntity.java
@@ -0,0 +1,18 @@
+package br.ecommerce.checkout.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class CheckoutEntity {
+
+    @Id
+    private Long id;
+
+    @Column
+    private String code;
+}
