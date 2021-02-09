6d8277bf18b931e81f4b0af9b387fad9782be7ae
Edevar Bastreghi Filho <edevarfilho@gmail.com>
2021-02-09 11:59:16 -0300

    CheckoutRepository
    CheckoutRepository

--- checkout/src/main/java/br/ecommerce/checkout/entity/CheckoutEntity.java
+++ checkout/src/main/java/br/ecommerce/checkout/entity/CheckoutEntity.java
@@ -1,18 +1,28 @@
 package br.ecommerce.checkout.entity;
 
+import lombok.Builder;
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+@Builder
 @Data
 @Entity
 public class CheckoutEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column
     private String code;
+
+    @Column
+    @Enumerated(value = EnumType.STRING)
+    public Status status;
+
+    public enum Status{
+        CREATED,
+        APPROVED
+    }
 }
