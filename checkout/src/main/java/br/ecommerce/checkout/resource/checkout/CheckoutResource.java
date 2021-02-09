d78ca943f5d0b3a46551e5406a54cffd8029265a
Edevar Bastreghi Filho <edevarfilho@gmail.com>
2021-02-09 12:00:10 -0300

    ChckoutResource status
    ChckoutResource status

---
+++ checkout/src/main/java/br/ecommerce/checkout/resource/checkout/CheckoutResource.java
@@ -0,0 +1,30 @@
+package br.ecommerce.checkout.resource.checkout;
+
+import br.ecommerce.checkout.entity.CheckoutEntity;
+import br.ecommerce.checkout.event.CheckoutCreatedEvent;
+import br.ecommerce.checkout.service.CheckoutService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/checkout")
+@RequiredArgsConstructor
+public class CheckoutResource {
+
+    private final CheckoutService checkoutService;
+
+    @PostMapping("/")
+    public ResponseEntity<CheckoutResponse> create(@RequestBody CheckoutRequest checkoutRequest){
+        final CheckoutEntity checkoutEntity = checkoutService.create(checkoutRequest)
+                .orElseThrow();
+        final CheckoutResponse checkoutResponse = CheckoutResponse.builder()
+                .code(checkoutEntity.getCode())
+                .build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(checkoutResponse);
+    }
+}
