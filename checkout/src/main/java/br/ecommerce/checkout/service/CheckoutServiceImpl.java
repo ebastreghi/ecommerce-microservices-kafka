cabe192e1417313e0da5f78b77befed8fc616cd2
Edevar Bastreghi Filho <edevarfilho@gmail.com>
2021-02-11 07:30:25 -0300

    sending checkoutCreatedEvent
    sending checkoutCreatedEvent

--- checkout/src/main/java/br/ecommerce/checkout/service/CheckoutServiceImpl.java
+++ checkout/src/main/java/br/ecommerce/checkout/service/CheckoutServiceImpl.java
@@ -1,9 +1,12 @@
 package br.ecommerce.checkout.service;
 
 import br.ecommerce.checkout.entity.CheckoutEntity;
+import br.ecommerce.checkout.event.CheckoutCreatedEvent;
 import br.ecommerce.checkout.repository.CheckoutRepository;
 import br.ecommerce.checkout.resource.checkout.CheckoutRequest;
+import br.ecommerce.checkout.streaming.CheckoutCreatedSource;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -12,9 +15,10 @@ import java.util.UUID;
 @Service
 //create constructor for all final attributes
 @RequiredArgsConstructor
-public class CheckoutServiceImpl implements CheckoutService{
+public class CheckoutServiceImpl implements CheckoutService {
 
     private final CheckoutRepository checkoutRepository;
+    private final CheckoutCreatedSource checkoutCreatedSource;
 
     @Override
     public Optional<CheckoutEntity> create(CheckoutRequest checkoutRequest) {
@@ -22,6 +26,13 @@ public class CheckoutServiceImpl implements CheckoutService{
                 .code(UUID.randomUUID().toString())
                 .status(CheckoutEntity.Status.CREATED)
                 .build();
+
+        final CheckoutEntity entity = checkoutRepository.save(checkoutEntity);
+        final CheckoutCreatedEvent checkoutCreatedEvent = CheckoutCreatedEvent.newBuilder()
+                .setCheckoutCode(entity.getCode())
+                .setStatus(entity.getStatus().name())
+                .build();
+        checkoutCreatedSource.output().send(MessageBuilder.withPayload(checkoutCreatedEvent).build());
         return Optional.of(checkoutRepository.save(checkoutEntity));
     }
 }
