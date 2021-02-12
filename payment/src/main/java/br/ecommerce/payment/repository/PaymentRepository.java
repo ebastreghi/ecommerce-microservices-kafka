34d69dd792428c806ccf1d2932f0b6ef3714a26e
Edevar Bastreghi Filho <edevarfilho@gmail.com>
2021-02-12 06:09:04 -0300

    PaymentEntity and repository
    PaymentEntity and repository

---
+++ payment/src/main/java/br/ecommerce/payment/repository/PaymentRepository.java
@@ -0,0 +1,7 @@
+package br.ecommerce.payment.repository;
+
+import br.ecommerce.payment.entity.PaymentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<PaymentEntity, Long> {
+}
